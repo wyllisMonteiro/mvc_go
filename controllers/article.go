ed8c98992e2f9f6c7267ce9d8fd8ee3af078bf26
Wyllis Monteiro <wyllismonteiro@gmail.com>
2020-08-08 16:31:49 +0200

    ci: generate artifats

--- controllers/article.go
+++ controllers/article.go
@@ -28,6 +28,9 @@ func DownloadArticles(w http.ResponseWriter, req *http.Request) {
 	}
 
 	err := service.LaunchExport(typeDownload, handler)
+	/*if err != nil {
+		return
+	}*/
 }
 
 // Render an article view
